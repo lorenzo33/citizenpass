175ca3d508f6ecd187a7cef69e074108ad9c636f
Laurent DELAPLACE <ldlp33@gmail.com>
2017-08-22 11:33:54 +0200

    creation de la classe lcd

--- borne_piscine/lcd_display.py
+++ borne_piscine/lcd_display.py
@@ -1,37 +1,19 @@
+# -*- coding:utf-8 -*-
 #!/usr/bin/python
 #
-# HD44780 LCD Test Script for
+# HD44780 LCD 
 # Raspberry Pi
 #
-# Author : Matt Hawkins
-# Site   : http://www.raspberrypi-spy.co.uk
+# Author : Laurent DELAPLACE
 # 
-# Date   : 03/08/2012
+# Date   : 17/08/2017
 #
 
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
-
-#import
+#importation des modules
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO to LCD mapping (PIN number)
+# Definition des constantes pour la connexion GPIO => LCD 
 LCD_RS = 37
 LCD_E  = 35
 LCD_D4 = 33 
@@ -40,141 +22,116 @@ LCD_D6 = 29
 LCD_D7 = 32
 LED_ON = 15
 
-# Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
+#Constantes pour l'affichage
+LCD_WIDTH = 16    
+LCD_CHR = True   #Mode Affichage
+LCD_CMD = False  #Mode Commande
 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line 
+LCD_LINE_1 = 0x80 # Adresse RAM pour 1ere ligne du LCD
+LCD_LINE_2 = 0xC0 # Adresse RAM pour 2eme ligne du LCD 
 
-# Timing constants
+#Constantes pour le temps
 E_PULSE = 0.00005
 E_DELAY = 0.00005
 
-def main():
-  # Main program block
+###################################################################
+# Déclaration de la classe lcd pour la gestion de l'afficheur
+###################################################################
+class lcd(object):
+    def __init__(self,rs,en,d4,d5,d6,d7,backlight):
+	
+	#Initialisation des variables
+	self._rs = rs
+	self._en = en
+	self._d4 = d4
+	self._d5 = d5
+	self._d6 = d6
+	self._d7 = d7
+	self._backlight = backlight
 
-  # Initialise display
-  lcd_init()
+	GPIO.setmode(GPIO.BOARD)     # Use pin numbers 
+	
+	#Initialise les branches GPIO en sortie
+	for pin in (rs,en,d4,d5,d6,d7,backlight):
+	    GPIO.setup(pin, GPIO.OUT)
+	self.lcd_clear()
 
-  # Toggle backlight on-off-on
-  GPIO.output(LED_ON, True)
-  time.sleep(1)
-  GPIO.output(LED_ON, False)
-  time.sleep(1)
-  GPIO.output(LED_ON, True)
-  time.sleep(1)
+    #Fonction d'initialisation de l'affichage
+    def lcd_clear(self):
+  	self.lcd_byte(0x33,LCD_CMD)
+  	self.lcd_byte(0x32,LCD_CMD)
+  	self.lcd_byte(0x28,LCD_CMD)
+  	self.lcd_byte(0x0C,LCD_CMD)
+  	self.lcd_byte(0x06,LCD_CMD)
+  	self.lcd_byte(0x01,LCD_CMD)
 
-  # Send some centred test
-  lcd_byte(LCD_LINE_1, LCD_CMD)
-  lcd_string("Rasbperry Pi",2)
-  lcd_byte(LCD_LINE_2, LCD_CMD)
-  lcd_string("Model B",2)
 
-  time.sleep(3) # 3 second delay
+    #Fonction d'affichage d'un message sur l'afficheur
+    def lcd_string(self,message,style):
+	# style=1 Left justified
+    	if style==1:
+	    message = message.ljust(LCD_WIDTH," ")  
 
-  # Send some left justified text
-  lcd_byte(LCD_LINE_1, LCD_CMD)
-  lcd_string("1234567890123456",1)
-  lcd_byte(LCD_LINE_2, LCD_CMD)
-  lcd_string("abcdefghijklmnop",1)
+	# style=2 Centred
+	elif style==2:
+	    message = message.center(LCD_WIDTH," ")
 
-  time.sleep(3) # 3 second delay
+	# style=3 Right justified
+	elif style==3:
+	    message = message.rjust(LCD_WIDTH," ")
 
-  # Send some right justified text
-  lcd_byte(LCD_LINE_1, LCD_CMD)
-  lcd_string("Raspberrypi-spy",3)
-  lcd_byte(LCD_LINE_2, LCD_CMD)
-  lcd_string(".co.uk",3)
+	for i in range(LCD_WIDTH):
+    	    self.lcd_byte(ord(message[i]),LCD_CHR)
 
-  time.sleep(30)
+    #Fonction qui traite les données envoyées
+    def lcd_byte(self,bits, mode):
+        # Send byte to data pins
+	# bits = data
+	# mode = True  for character
+	# False for command
 
-  # Turn off backlight
-  GPIO.output(LED_ON, False)
+	GPIO.output(self._rs, mode) # RS
 
-def lcd_init():
-  GPIO.setmode(GPIO.BOARD)       # Use pin numbers 
-  GPIO.setup(LCD_E, GPIO.OUT)  # E
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
-  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-  GPIO.setup(LED_ON, GPIO.OUT) # Backlight enable  
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD)
-  lcd_byte(0x32,LCD_CMD)
-  lcd_byte(0x28,LCD_CMD)
-  lcd_byte(0x0C,LCD_CMD)  
-  lcd_byte(0x06,LCD_CMD)
-  lcd_byte(0x01,LCD_CMD)  
+	# High bits
+	GPIO.output(self._d4, False)
+	GPIO.output(self._d5, False)
+	GPIO.output(self._d6, False)
+	GPIO.output(self._d7, False)
+	
+	if bits&0x10==0x10:
+	    GPIO.output(self._d4, True)
+	if bits&0x20==0x20:
+	    GPIO.output(self._d5, True)
+	if bits&0x40==0x40:
+	    GPIO.output(self._d6, True)
+	if bits&0x80==0x80:
+	    GPIO.output(self._d7, True)
 
-def lcd_string(message,style):
-  # Send string to display
-  # style=1 Left justified
-  # style=2 Centred
-  # style=3 Right justified
+	# Toggle 'Enable' pin
+	time.sleep(E_DELAY)    
+	GPIO.output(self._en, True)  
+	time.sleep(E_PULSE)
+	GPIO.output(self._en, False)  
+	time.sleep(E_DELAY)      
 
-  if style==1:
-    message = message.ljust(LCD_WIDTH," ")  
-  elif style==2:
-    message = message.center(LCD_WIDTH," ")
-  elif style==3:
-    message = message.rjust(LCD_WIDTH," ")
+	# Low bits
+	GPIO.output(self._d4, False)
+	GPIO.output(self._d5, False)
+	GPIO.output(self._d6, False)
+	GPIO.output(self._d7, False)
+	
+	if bits&0x01==0x01:
+	    GPIO.output(self._d4, True)
+	if bits&0x02==0x02:
+	    GPIO.output(self._d5, True)
+	if bits&0x04==0x04:
+	    GPIO.output(self._d6, True)
+	if bits&0x08==0x08:
+	    GPIO.output(self._d7, True)
 
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
-
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = data
-  # mode = True  for character
-  #        False for command
-
-  GPIO.output(LCD_RS, mode) # RS
-
-  # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  time.sleep(E_DELAY)    
-  GPIO.output(LCD_E, True)  
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)  
-  time.sleep(E_DELAY)      
-
-  # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  time.sleep(E_DELAY)    
-  GPIO.output(LCD_E, True)  
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)  
-  time.sleep(E_DELAY)   
-
-if __name__ == '__main__':
-  main()
+	# Toggle 'Enable' pin
+	time.sleep(E_DELAY)    
+	GPIO.output(self._en, True)  
+	time.sleep(E_PULSE)
+	GPIO.output(self._en, False)  
+	time.sleep(E_DELAY)
